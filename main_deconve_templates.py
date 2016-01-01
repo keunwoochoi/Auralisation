0cb5bebb822e1a6cbc1f5c5656efc553a4610db1
keunwoochoi <gnuchoi+github@gmail.com>
2016-01-01 20:58:52 +0000

    multiprocessing

--- main_deconve_templates.py
+++ main_deconve_templates.py
@@ -19,6 +19,55 @@ from multiprocessing import Pool
 -- [1-3] 'separate' or 'auralise' the deconved spectrogram using phase information of original signal.
 - [2]listen up!
 '''
+print '--- prepare files ---'
+path_src = 'src_templates/'
+path_SRC = 'src_templates_stft/'
+path_results = 'results/'
+if not os.path.exists(path_results):
+	os.makedirs(path_results)
+depths = [5,4,3,2,1]
+
+def deconv_a_file(filename):
+
+	song_id = filename.split('.')[0]
+	path_out_here = path_results + song_id + '/'
+	path_img_here = path_results + song_id + '_img/'
+	SRC = np.load(path_SRC + filename)
+	if os.path.exists(path_out_here) and os.path.exists(path_img_here):
+		print '%s might be done already, I skip this.' % song_id
+		print 'remove %s and %s to proceed.' % (path_out_here, path_img_here)
+		continue
+	if not os.path.exists(path_out_here):
+		os.makedirs(path_out_here)	
+	if not os.path.exists(path_img_here):
+		os.makedirs(path_img_here)
+	filename_out = '%s_a_original.wav' % (song_id)	
+	librosa.output.write_wav(path_out_here + filename_out, librosa.istft(SRC, hop_length=N_FFT/2), 
+								sr=SAMPLE_RATE, 
+								norm=True)
+
+	for depth in depths:	
+		print '--- deconve! ---'
+		deconvedMASKS = auralise.get_deconve_mask(W[:depth], layer_names, SRC, depth) # size can be smaller than SRC due to downsampling
+
+		print 'result; %d masks with size of %d, %d' % deconvedMASKS.shape
+
+		for deconved_feature_ind, deconvedMASK_here in enumerate(deconvedMASKS):
+
+			MASK = np.zeros(SRC.shape)
+			MASK[0:deconvedMASK_here.shape[0], 0:deconvedMASK_here.shape[1]] = deconvedMASK_here
+			deconvedSRC = np.multiply(SRC, MASK)
+
+			filename_out = '%s_deconved_from_depth_%d_feature_%d.wav' % (song_id, depth, deconved_feature_ind)
+			librosa.output.write_wav(path_out_here + filename_out, librosa.istft(deconvedSRC, hop_length=N_FFT/2), 
+								sr=SAMPLE_RATE, 
+								norm=True)
+			filename_img_out = 'spectrogram_%s_from_depth_%d_feature_%d.png' % (song_id, depth, deconved_feature_ind)
+			imsave(path_img_here + filename_img_out , np.flipud(np.multiply(np.abs(SRC), MASK)))
+
+			filename_img_out = 'filter_for_%s_from_depth_%d_feature_%d.png' % (song_id, depth, deconved_feature_ind)
+			imsave(path_img_here + filename_img_out , np.flipud(MASK))
+
 
 if __name__ == "__main__":
 	'''
@@ -29,19 +78,12 @@ if __name__ == "__main__":
 	, where the deconvolution functions are used.
 
 	'''
+
 	# load learned weights
 	W, layer_names = auralise.load_weights()
 	num_conv_layer = len(W)
 
 	# load files
-	print '--- prepare files ---'
-	
-	path_src = 'src_templates/'
-	path_SRC = 'src_templates_stft/'
-	path_results = 'results/'
-	if not os.path.exists(path_results):
-		os.makedirs(path_results)
-
 	filenames_src = os.listdir(path_src)
 	filenames_src = [filename for filename in filenames_src if filename.split('.')[1] in ['wav', 'mp3']]
 	
@@ -70,45 +112,7 @@ if __name__ == "__main__":
 
 	# deconve
 
-	depths = [5,4,3,2,1]
-
-	for filename in filenames_SRC:
-		song_id = filename.split('.')[0]
-		path_out_here = path_results + song_id + '/'
-		path_img_here = path_results + song_id + '_img/'
-		SRC = np.load(path_SRC + filename)
-		if os.path.exists(path_out_here) and os.path.exists(path_img_here):
-			print '%s might be done already, I skip this.' % song_id
-			print 'remove %s and %s to proceed.' % (path_out_here, path_img_here)
-			continue
-		if not os.path.exists(path_out_here):
-			os.makedirs(path_out_here)	
-		if not os.path.exists(path_img_here):
-			os.makedirs(path_img_here)
-		filename_out = '%s_a_original.wav' % (song_id)	
-		librosa.output.write_wav(path_out_here + filename_out, librosa.istft(SRC, hop_length=N_FFT/2), 
-									sr=SAMPLE_RATE, 
-									norm=True)
-
-		for depth in depths:	
-			print '--- deconve! ---'
-			deconvedMASKS = auralise.get_deconve_mask(W[:depth], layer_names, SRC, depth) # size can be smaller than SRC due to downsampling
-
-			print 'result; %d masks with size of %d, %d' % deconvedMASKS.shape
-
-			for deconved_feature_ind, deconvedMASK_here in enumerate(deconvedMASKS):
-
-				MASK = np.zeros(SRC.shape)
-				MASK[0:deconvedMASK_here.shape[0], 0:deconvedMASK_here.shape[1]] = deconvedMASK_here
-				deconvedSRC = np.multiply(SRC, MASK)
-
-				filename_out = '%s_deconved_from_depth_%d_feature_%d.wav' % (song_id, depth, deconved_feature_ind)
-				librosa.output.write_wav(path_out_here + filename_out, librosa.istft(deconvedSRC, hop_length=N_FFT/2), 
-									sr=SAMPLE_RATE, 
-									norm=True)
-				filename_img_out = 'spectrogram_%s_from_depth_%d_feature_%d.png' % (song_id, depth, deconved_feature_ind)
-				imsave(path_img_here + filename_img_out , np.flipud(np.multiply(np.abs(SRC), MASK)))
-
-				filename_img_out = 'filter_for_%s_from_depth_%d_feature_%d.png' % (song_id, depth, deconved_feature_ind)
-				imsave(path_img_here + filename_img_out , np.flipud(MASK))
+	p = Pool(48)
+	p.map(deconv_a_file, filenames_SRC)
+		
 				
